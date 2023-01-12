01837c45ee4ef2eab4fdbd38819eded30a8865d6
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-01-12 13:07:53 +0100

    Add private C API definitions

---
+++ src/jsc.h
@@ -0,0 +1,15 @@
+#ifndef JSC_H
+#define JSC_H
+
+#include "jsc/JSBasePrivate.h"
+#include "jsc/JSContextRefPrivate.h"
+#include "jsc/JSHeapFinalizerPrivate.h"
+#include "jsc/JSLockRefPrivate.h"
+#include "jsc/JSMarkingConstraintPrivate.h"
+#include "jsc/JSObjectRefPrivate.h"
+#include "jsc/JSScriptRefPrivate.h"
+#include "jsc/JSStringRefPrivate.h"
+#include "jsc/JSWeakObjectMapRefPrivate.h"
+#include "jsc/JSWeakPrivate.h"
+
+#endif // JSC_H
